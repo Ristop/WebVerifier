75c7a7c9f4afea3e9229deb7c4e1058f916a4b0a
Risto Pärnapuu <ristoparnapuu@gmail.com>
2016-07-08 16:28:26 +0300

    Added log, fixed stop button, implemented alert times, refactored some code

--- src/Controller.java
+++ src/Controller.java
@@ -1,28 +1,100 @@
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Timer;
 
 public class Controller {
-    @FXML private TextField URLField;
-    @FXML private TextField ContentField;
-    @FXML private TextField IntervalField;
-    @FXML private TextField MaxResponseTimeField;
-    @FXML private TextField MessageSendOutTimesField;
-    @FXML private TextField CellNumberField;
+    @FXML
+    private TextField URLField;
+    @FXML
+    private TextField ContentField;
+    @FXML
+    private TextField IntervalField;
+    @FXML
+    private TextField MaxResponseTimeField;
+    @FXML
+    private TextField MessageSendOutTimesField;
+    @FXML
+    private TextField CellNumberField;
 
-    @FXML private Button StartButton;
+    @FXML
+    private Button StartButton;
 
-    @FXML private void startChecking(){
-        Timer timer = new Timer();
+    @FXML
+    private TextArea logArea;
 
-        String url = URLField.getText();
-        String content = ContentField.getText();
-        Long period = Long.parseLong(IntervalField.getText())*1000;
-        Double timeout = Double.parseDouble(MaxResponseTimeField.getText())*1000;
+    private boolean checking = false;
+    private ArrayList<Integer> alertConditions = new ArrayList<>();
+    private Timer timer;
 
-        timer.scheduleAtFixedRate(new ResponseCheckerTask(url, content, timeout.intValue()), 0, period);
+    @FXML
+    private void startChecking() {
+        if (!checking) {
+            if (validateFields()){
+                timer = new Timer();
+                String url = URLField.getText();
+                String content = ContentField.getText();
+                Long period = Long.parseLong(IntervalField.getText()) * 1000;
+                Double timeout = Double.parseDouble(MaxResponseTimeField.getText()) * 1000;
+                String cellnumber = CellNumberField.getText();
+
+                timer.scheduleAtFixedRate(new ResponseCheckerTask(url, content, timeout.intValue(), logArea, alertConditions, cellnumber), 0, period);
+
+                StartButton.setText("Stop");
+                checking = true;
+            }
+        } else {
+            timer.cancel();
+            timer.purge();
+            checking = false;
+            StartButton.setText("Start");
+        }
+    }
+
+    private boolean validateFields() {
+        boolean validated = true;
+        if (!URLField.getText().startsWith("http://")) {
+            URLField.setStyle("-fx-border-color: red;");
+            validated = false;
+        }else {
+            URLField.setStyle("-fx-border-color: gray;");
+        }
+
+        try {
+            Long.parseLong(IntervalField.getText());
+            IntervalField.setStyle("-fx-border-color: gray;");
+        }catch (Exception e){
+            IntervalField.setStyle("-fx-border-color: red;");
+            validated = false;
+        }
+
+        try {
+            Double.parseDouble(MaxResponseTimeField.getText());
+            MaxResponseTimeField.setStyle("-fx-border-color: gray;");
+        }catch (Exception e){
+            MaxResponseTimeField.setStyle("-fx-border-color: red;");
+            validated = false;
+        }
+
+        try{
+            String messageSendOutTimesString = MessageSendOutTimesField.getText().replace(" ", "");
+            String[] split = messageSendOutTimesString.split(",");
+            List<String> times = new ArrayList<>(Arrays.asList(split));
+            for (String time : times) {
+                alertConditions.add(Integer.parseInt(time));
+            }
+            MessageSendOutTimesField.setStyle("-fx-border-color: gray;");
+        } catch (Exception e){
+            MessageSendOutTimesField.setStyle("-fx-border-color: red;");
+        }
+
+        return validated;
     }
 
 }
--- src/ResponseCheckerTask.java
+++ src/ResponseCheckerTask.java
@@ -1,4 +1,12 @@
+import javafx.scene.control.TextArea;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.TimerTask;
 
 /**
@@ -9,22 +17,78 @@ public class ResponseCheckerTask extends TimerTask {
     private String url;
     private String content;
     private int timeout;
+    private ArrayList<Integer> alertConditions;
+    private String cellNumber;
+    private TextArea logArea;
 
-    public ResponseCheckerTask(String url, String content, int timeout) {
-        System.out.println("Initializing response checker task");
+    private int connectionFailures = 0;
+
+    public ResponseCheckerTask(String url, String content, int timeout, ArrayList<Integer> alertConditions, String cellNumber, TextArea logArea) {
         this.url = url;
         this.content = content;
         this.timeout = timeout;
+        this.alertConditions = alertConditions;
+        this.cellNumber = cellNumber;
+        this.logArea = logArea;
     }
 
     @Override
     public void run() {
-        System.out.println("Executing response checker task");
-        WebChecker wc = new WebChecker();
         try {
-            wc.getResponse(url, content, timeout);
+            getResponse(timeout);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void getResponse(int timeout) throws IOException {
+        URL url = new URL(this.url);
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setConnectTimeout(timeout);
+
+        InputStream is;
+        try {
+            long startTime = System.nanoTime();
+            is = con.getInputStream();
+            Double endTime = ((Double) ((System.nanoTime() - startTime) / 10000000.0)).intValue() / 100.0;
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            checkResponseContent(br, endTime);
+        } catch (IOException e) {
+            logArea.appendText("request to " + this.url + " - timeout");
+            addFailure();
+        }
+
+    }
+
+    private void checkResponseContent(BufferedReader br, double endTime) throws IOException {
+        boolean correctContent = false;
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (line.contains(this.content)) {
+                correctContent = true;
+                break;
+            }
+        }
+
+        if (correctContent) {
+            logArea.appendText("request to " + url + " - returned 200 in " + endTime + " ns\n");
+            connectionFailures = 0;
+        } else {
+            logArea.appendText("request to " + url + " - returned 200 but did not include required string: " + this.content);
+            addFailure();
+        }
+
+        br.close();
+    }
+
+    private void addFailure() {
+        connectionFailures++;
+        if (alertConditions.contains(connectionFailures)){
+            logArea.appendText(" - sending out SMS alert to " + this.cellNumber + "\n");
+        } else {
+            logArea.appendText("\n");
         }
     }
 }
